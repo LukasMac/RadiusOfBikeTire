2d40861ab809a371ce0f9717e4dc179ef33dd6c4
Lukas <lukas.maciulis@gmail.com>
2013-06-08 14:29:44 +0300

    Changed folder structure. Ported class constructor to separate method.

--- test/tc_radius_of_bike_tire.rb
+++ test/tc_radius_of_bike_tire.rb
@@ -1,22 +1,26 @@
-require '../RadiusOfBikeTire/radius_of_bike_tire'
 require 'test/unit'
+require_relative '../src/radius_of_bike_tire'
 
 class TestRadiusOfBikeTire < Test::Unit::TestCase
+  def radius_of_bike_tire
+    RadiusOfBikeTire.new
+  end
+
   def test_get_radius_from_length_zeroLength_returnsZero
-    result = RadiusOfBikeTire.new.get_radius_from_length(0)
+    result = radius_of_bike_tire.get_radius_from_length(0)
 
     assert_equal result, 0
   end
 
   def test_get_radius_from_length_lengthOne_returns0159
-    result = RadiusOfBikeTire.new.get_radius_from_length(1)
+    result = radius_of_bike_tire.get_radius_from_length(1)
 
-    assert_equal result.round(3), 0.159
+    assert_in_delta(0.159, result, 3)
   end
 
   def test_get_radius_from_length_negativeLength_risesArgumentError
     assert_raise ArgumentError do
-      RadiusOfBikeTire.new.get_radius_from_length(-1)
+      radius_of_bike_tire.get_radius_from_length(-1)
     end
   end
 end
